758f2e7834eb9b487ff17234d856742886ae36e9
grodoa11 <grodoa11@htlkaindorf.at>
2015-04-23 15:54:49 +0200

    wetterliste

--- CheckYourWeather/Web/Wetterstationenliste.cs
+++ CheckYourWeather/Web/Wetterstationenliste.cs
@@ -5,7 +5,84 @@ using System.Web;
 
 namespace Web
 {
-    public class Wetterstationenliste
+    public class Wetterstationenliste:IList<Wetterstation>
     {
+        private List<Wetterstation> list;
+
+        public Wetterstationenliste()
+        {
+            list = new List<Wetterstation>();
+        }
+        public int IndexOf(Wetterstation item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, Wetterstation item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Wetterstation this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                list[index] = value;
+            }
+        }
+
+        public void Add(Wetterstation item)
+        {
+            list.Add(item);
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Wetterstation item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Wetterstation[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { return list.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(Wetterstation item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Wetterstation> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
