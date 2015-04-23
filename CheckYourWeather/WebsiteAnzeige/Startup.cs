ca5a02e620c3d24de8c5d0e11bab55d6b48320ca
samdoa11 <samdoa11@htlkaindorf.at>
2015-04-23 14:26:49 +0200

    rf

---
+++ CheckYourWeather/WebsiteAnzeige/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebsiteAnzeige.Startup))]
+namespace WebsiteAnzeige
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
