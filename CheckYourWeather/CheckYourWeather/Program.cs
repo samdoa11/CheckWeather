1af11bfba15ce70405c70c62ad63d172c2d3e081
samdoa11 <samdoa11@htlkaindorf.at>
2015-05-10 15:10:42 +0200

    Auslesen der Ids

--- CheckYourWeather/CheckYourWeather/Program.cs
+++ CheckYourWeather/CheckYourWeather/Program.cs
@@ -2,25 +2,115 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Web.UI.HtmlControls;
+using System.Windows.Forms;
 
 namespace CheckYourWeather
 {
     public class Program
     {
+        public const string TestUrl = "http://app.luis.steiermark.at/luft2/suche.php";
+
+        private List<int> m_Ids;
+        public Program()
+        {
+            
+        }
+
+        [STAThread]
         public static void Main(string[] args)
         {
-            Console.WriteLine();
-            //kjhkjh
+            //Ids der Wetterstationen die es gibt
+            LandSteiermark stmk = new LandSteiermark("station1");
+            List<int> idList = stmk.getIds();
+
+            //Ids der Komponenten die es gibt
+            stmk = new LandSteiermark("komponente1");
+            List<int> idListKomponente = stmk.getIds();
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey(true);
+
         }
     }
 
     public class LandSteiermark {
-        public int getIds()
-        {
+        //Members
+        public const string TestUrl = "http://app.luis.steiermark.at/luft2/suche.php";
+        private List<int> m_Ids;
+        private String id;
 
-            return 1;
+        public LandSteiermark(String id)
+        {
+            this.id = id;
+        }
+
+        /// <summary>
+        /// Author: Dominik Sammer
+        /// Methode die eine Liste der gesamten Ids der Wetterstationen zurückgibt.
+        /// Die benötigt man später zum zusammenbauen des Links für den Download.
+        /// Die Aufrufende Methode muss ein STATThreat sein!
+        /// @Return: Liste von Ints, bei Fehler wird eine Leere Liste zurückgegeben.
+        /// </summary>
+
+        public List<int> getIds()
+        {
+            WebBrowser wb = new WebBrowser();
+            Program p = new Program();
+            try
+            {
+                wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(this.wb_DocumentCompleted);
+                wb.Navigate(TestUrl);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return new List<int>();
+            }
+
+            while (wb.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+
+
+            return this.m_Ids;
+        }
+
+
+        public void wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            try
+            {
+                WebBrowser wb = (WebBrowser)sender;
+
+                HtmlElement document = wb.Document.GetElementsByTagName("html")[0];
+
+                HtmlElement element = wb.Document.GetElementById(this.id);
+
+                List<int> idList = new List<int>();
+                String s1 = element.InnerHtml;
+                String[] split = s1.Split(' ');
+                foreach (String teil in split)
+                {
+                    if (teil.Contains("value"))
+                    {
+                        String value = teil.Split('=')[1];
+                        value = value.Split('>')[0];
+                        if (value != "\"\"")
+                        {
+                            idList.Add(Convert.ToInt32(value));
+                        }
+                    }
+                }
+                this.m_Ids = idList;
+            }
+            catch (Exception ex)
+            {
+               
+            }
         }
     }
+
+
 }
